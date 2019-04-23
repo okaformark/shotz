b7db4b77d00cd0bc88dce1d3d09ee3558dab1ece
Mark Okafor <okaformark@gmail.com>
2019-04-22 19:30:46 -0500

    bg color filter added

--- src/javascripts/component/locations/locations.js
+++ src/javascripts/component/locations/locations.js
@@ -4,6 +4,27 @@ import './locations.scss';
 
 let movieLocations = [];
 
+
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
 const domStringBuilder = () => {
   let domString = '';
   domString += '<div class = "row">';
@@ -11,10 +32,10 @@ const domStringBuilder = () => {
     domString += '<div class = "col-2">';
     domString += '<div class="card border-primary mb-3" style="max-width: 18rem;">';
     domString += ` <img src="${location.imageUrl}" class="card-img-top">`;
-    domString += `<div class='card-header d-flex justify-content-center'>${location.id}</div>`;
+    domString += `<div class='card-header d-flex justify-content-center font-weight-bold'>${location.id}</div>`;
     domString += '<div class="card-body text-primary">';
-    domString += `<h5 class="card-title card-header"> ${location.name}</h5>`;
-    domString += `<button type = "button" class = "btn btn-danger"><p class="card-text"> ${location.address}</p></button>`;
+    domString += `<h5 class="card-title card-header ${shootTimeClass(location.shootTime)}"> ${location.name}</h5>`;
+    domString += `<button type = "button" class = "btn btn-dark"><p class="card-text"> ${location.address}</p></button>`;
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';

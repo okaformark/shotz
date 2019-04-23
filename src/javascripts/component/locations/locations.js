7c9ad06efea7b6ec374a456f7e4ffd64f3ae82a2
Mark Okafor <okaformark@gmail.com>
2019-04-22 19:16:11 -0500

    location card

--- src/javascripts/component/locations/locations.js
+++ src/javascripts/component/locations/locations.js
@@ -6,8 +6,18 @@ let movieLocations = [];
 
 const domStringBuilder = () => {
   let domString = '';
+  domString += '<div class = "row">';
   movieLocations.forEach((location) => {
-    domString += `<h2>${location.name}</h2>`;
+    domString += '<div class = "col-2">';
+    domString += '<div class="card border-primary mb-3" style="max-width: 18rem;">';
+    domString += ` <img src="${location.imageUrl}" class="card-img-top">`;
+    domString += `<div class='card-header d-flex justify-content-center'>${location.id}</div>`;
+    domString += '<div class="card-body text-primary">';
+    domString += `<h5 class="card-title card-header"> ${location.name}</h5>`;
+    domString += `<button type = "button" class = "btn btn-danger"><p class="card-text"> ${location.address}</p></button>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
   });
   util.printToDom('locations', domString);
 };
